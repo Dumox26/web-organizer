bdc094e0c366b188d058fa67b02c0f05c90ba97f
Dumox <semafor26@gmail.com>
2019-10-10 12:38:58 +0200

    Redesign html, delete bs4
    add menu and search

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const x = document.querySelector('.hamburger-btn');
 const y = document.querySelector('.site-header__navigation');
+const z = document.querySelector('.form-btn');
+const v = document.querySelector('.site-header__form');
 x.addEventListener('click', () => {
   x.classList.toggle('hamburger-btn--active');
   y.classList.toggle('site-header__navigation--active');
 });
+
+z.addEventListener('click', () => {
+  v.classList.toggle('site-header__form--active');
+})
