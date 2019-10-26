17f3c52dfa5fae1c25a92081e8cffb5392ead436
Dumox <semafor26@gmail.com>
2019-10-26 20:09:47 +0200

    create new file stickersClass
    stickerClass is responsible for keeping all stickers and create html
    object who represent sticker.
    Stickerss class is allso responsible for fill sticker with data.
    StickerClass from now represent the individual sticker and its
    functionality

--- js/main.js
+++ js/main.js
@@ -13,13 +13,34 @@ const handleSearchClick = () => {
   siteHeaderForm.classList.toggle('site-header__form--active');
 };
 
+const createSticker = (stickerTitleValue, stickerDateValue, stickerTimeValue, stickerTextValue) => {
+  const sticker = new StickerClass(stickerTitleValue, stickerDateValue,
+    stickerTimeValue, stickerTextValue);
+  console.log(sticker);
+  sticker.saveStickerInLocalStorage();
+  sticker.createHtmlSticker();
+};
+
+const handleSubmitStickerClick = () => {
+  event.preventDefault();
+  const stickerFormTitleInput = document.querySelector('.add-sticker-menu__sticker-title-input');
+  const stickerFormDateInput = document.querySelector('.add-sticker-menu__sticker-date-input');
+  const stickerFormTimeInput = document.querySelector('.add-sticker-menu__sticker-time-input');
+  const stickerFormTextarea = document.querySelector('.add-sticker-menu__sticker-textarea');
+  console.log(stickerFormDateInput);
+  console.log('test');
+  createSticker(stickerFormTitleInput.value, stickerFormDateInput.value, stickerFormTimeInput.value,
+    stickerFormTextarea.value);
+};
+
 const handleAddStickerClick = () => {
   const addStickerMenu = document.querySelector('.add-sticker-menu');
   addStickerMenu.classList.toggle('add-sticker-menu--active');
-  const sticker = new StickerClass('12321', '1441');
-  console.log(sticker);
+  // const sticker = new StickerClass('12321', '1441');
 };
 
 document.querySelector('.hamburger-btn').addEventListener('click', handleMenuClick);
 document.querySelector('.form-btn').addEventListener('click', handleSearchClick);
 document.querySelector('#add-sticker').addEventListener('click', handleAddStickerClick);
+document.querySelectorAll('.site-header__btn').forEach((btn) => btn.addEventListener('click', handleMenuClick));
+document.querySelector('.add-sticker-menu__submit-sticker').addEventListener('click', handleSubmitStickerClick);
--- js/stickerClass.js
+++ js/stickerClass.js
@@ -4,6 +4,28 @@ class StickerClass {
     this.date = date;
     this.description = description;
   }
+
+  saveStickerInLocalStorage() {
+    try {
+      localStorage.setItem(this.title, JSON.stringify(this));
+      console.log('sukces');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  createHtmlSticker() {
+    const stickerCnt = document.querySelector('.main');
+    const stickerTemplate = document.querySelector('.sticker-template');
+    const newStickerHtml = stickerTemplate.cloneNode(true);
+    console.log(newStickerHtml);
+    newStickerHtml.classList.remove('sticker-template');
+    stickerCnt.appendChild(newStickerHtml);
+  }
+
+  fillHtmlSticker() {
+
+  }
 }
 
 export default StickerClass;
